b0e29e2fbe24a61a1e8d44e1c786af89fac0ffd6
Woong <jin02014@naver.com>
2024-05-22 23:58:00 +0900

    [level 1] Title: 바탕화면 정리, Time: 0.12 ms, Memory: 76 MB -BaekjoonHub

---
+++ 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.java
@@ -0,0 +1,48 @@
+class Solution {
+    public int[] solution(String[] wallpaper) {
+        int[] answer = new int [4];
+        
+        int [] start = new int [2];
+        int [] end = new int [2];
+        boolean first = true;
+        for (int y = 0; y < wallpaper.length; y++) {
+			char [] word = wallpaper[y].toCharArray();
+			for (int x = 0; x < word.length; x++) {
+				
+				if(word[x] == '#') {
+					if(first) {
+						start[0] = y;
+						start[1] = x;
+						end[0] = y;
+						end[1] = x;
+						first= false;
+					}else {
+						
+						if(end[0] < y) {
+							end[0] = y;
+						}
+						if(start[0] > y) {
+							start[0] = y;
+						}
+						
+						if(start[1] > x) {
+							start[1] = x;
+						}
+						if(end[1] < x) {
+							end[1] = x;
+						}
+					}
+					
+				}
+			}
+		}
+        
+        answer[0] = start[0];
+        answer[1] = start[1];
+        answer[2] = end[0]+1;
+        answer[3] = end[1]+1;
+
+        return answer;
+    }
+	
+}
